dce939cb889e1924a4d7217ce06b6cc20b9e1307
gsilva-co2hc <g.silva@co2hc.com>
2025-04-08 17:51:22 -0300

    add host lb

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,7 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     port: 3006,
-    allowedHosts: ['estoque-monami-lb-1077406287.us-east-1.elb.amazonaws.com','estoque.usemonami.com'],
+    allowedHosts: ['estoque-monami-lb-1077406287.us-east-1.elb.amazonaws.com','estoque.usemonami.com'], 
+      proxy: {
+      '/api': {
+        target: 'http://estoque.usemonami.com:3000', // URL do backend
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove o prefixo "/api"
+      },
+    },
   },
   plugins: [react()],
 })
