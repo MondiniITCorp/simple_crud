a1f7239005757d83ccd7f297b4eede179beffa4d
gsilva-co2hc <g.silva@co2hc.com>
2025-04-08 18:02:16 -0300

    add host lb

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -6,14 +6,14 @@ export default defineConfig({
   server: {
     port: 3006,
     allowedHosts: ['estoque-monami-lb-1077406287.us-east-1.elb.amazonaws.com','estoque.usemonami.com'], 
-    proxy: {
-    '/api': {
-      target: 'https://estoque.usemonami.com:3000', // URL do backend
-      changeOrigin: true,
-      secure: false,
-      rewrite: (path) => path.replace(/^\/api/, ''), // Remove o prefixo "/api"
-    },
-  },
+    // proxy: {
+    // '/api': {
+    //   target: 'https://estoque.usemonami.com:3000/', // URL do backend
+    //   changeOrigin: true,
+    //   secure: false,
+    //   rewrite: (path) => path.replace(/^\/api/, ''), // Remove o prefixo "/api"
+    // },
+  // },
   },
   plugins: [react()],
 })
