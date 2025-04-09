066916e251edbc3a4e70a61e4eba7633cecb27fa
gsilva-co2hc <g.silva@co2hc.com>
2025-04-09 13:22:50 -0300

    add METHOD cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['https://estoque.usemonami.com/'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
