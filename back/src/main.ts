5948776795db8e82cd1bcc0cca6c5ea7ea7621e2
gsilva-co2hc <g.silva@co2hc.com>
2025-04-09 11:09:11 -0300

    add security login

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['*'],
+    origin: ['https://estoque.usemonami.com/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
