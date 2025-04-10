9da6b9f918f1f0e51f0f8f694a2bbfc9da24d1bb
gsilva-co2hc <g.silva@co2hc.com>
2025-04-10 14:44:54 -0300

    config local database

--- back/src/ormconfig.ts
+++ back/src/ormconfig.ts
@@ -2,7 +2,7 @@ import { DataSourceOptions } from 'typeorm';
 
 export const config: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'host.docker.internal',
   port: 5432,
   username: 'pguser',
   password: 'pgpassword',
