450a1b0ce10308c0fe680480ef5d463a2ab5e9c0
gsilva-co2hc <g.silva@co2hc.com>
2025-04-10 15:02:01 -0300

    config local database

--- back/src/ormconfig.ts
+++ back/src/ormconfig.ts
@@ -2,7 +2,7 @@ import { DataSourceOptions } from 'typeorm';
 
 export const config: DataSourceOptions = {
   type: 'postgres',
-  host: '127.0.0.1',
+  host: ' 172.31.29.0',
   port: 5432,
   username: 'pguser',
   password: 'pgpassword',
